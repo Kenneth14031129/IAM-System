b1509e31312d20573d4c32bbdc5bde2c75a3b0aa
Kenneth14031129 <justken2000@gmail.com>
2025-08-26 17:04:44 +0800

    Add vercel in backend

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3001/api';
 
 axios.defaults.baseURL = API_BASE_URL;
 
